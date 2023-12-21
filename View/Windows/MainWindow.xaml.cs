43a9949284a8f89a5bb1924848eb078de1757b10
Laura <lavrusik515@gmail.com>
2023-12-21 23:18:39 +0300

    Добавьте файлы проекта.

---
+++ View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+    }
+}
